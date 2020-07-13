a615700a1cd45bb2a47e86ce8e010bad5821e989
dmitriy.klimkov <dmitriy.klimkov@rt.ru>
2020-07-13 07:46:01 +0300

    заменил радиобаттоны на чекбоксы + сделал, чтоб сразу открывались вопросы

--- pollme/urls.py
+++ pollme/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
     path('polls/', include('polls.urls', namespace="polls")),
     path('accounts/', include('accounts.urls', namespace="accounts")),
+]
+
+# Добавил URL соотношения, чтобы перенаправлять запросы с корневового URL, на URL приложения
+urlpatterns += [
+    path('', RedirectView.as_view(url='/polls/list/', permanent=True)),
 ]
